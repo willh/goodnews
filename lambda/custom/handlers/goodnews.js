90d697facbe42c0ed57b7eb9eeb5a1b338014949
willh <william.hamill@gmail.com>
2019-02-15 14:50:54 +0000

    Updating good news handler to call comprehend

--- lambda/custom/handlers/goodnews.js
+++ lambda/custom/handlers/goodnews.js
@@ -1,8 +1,8 @@
-// require news API
-// require AWS SDK
+const AWS = require('aws-sdk');
 const NewsAPI = require('newsapi');
 const newsApiKey = process.env.newsApiKey;
 const newsapi = new NewsAPI(newsApiKey);
+const comprehend = new AWS.Comprehend({apiVersion: '2017-11-27'});
 
 function canHandle(handlerInput) {
   return handlerInput.requestEnvelope.request.type === 'IntentRequest'
@@ -15,22 +15,43 @@ async function handle(handlerInput) {
   let newsHeadlines;
   await newsapi.v2.topHeadlines({
     sources: 'bbc-news,google-news-uk',
-    language: 'en'
+    language: 'en',
+    pageSize: 25
   }).then(response => {
     newsHeadlines = getHeadlineDescriptions(response);
   });
   console.log(newsHeadlines);
 
   // call sentiment analysis here, ditch the bad ones
+  let params = {
+    LanguageCode: "en",
+    TextList: newsHeadlines
+  };
 
-  // ditch the negative results... which might be a lot
-
-  // take the first 1-2 positive ones and add into the response
-
-  const speechText = "You asked for some good news";
-      return handlerInput.responseBuilder
-        .speak(speechText)
+  let sentimentResult;
+  try {
+    sentimentResult = await comprehend.batchDetectSentiment(params).promise();
+    console.log(JSON.stringify(sentimentResult));
+  } catch (error) {
+    console.log(`oh dear god no! ${error}`);
+    return handlerInput.responseBuilder
+        .speak("Sorry, something went horribly wrong")
         .getResponse();
+  }
+
+  let positiveResults = sentimentResult.resultList.filter(result => result.Sentiment == 'POSITIVE');
+
+  let topTwoPositiveResults = positiveResults.slice(0, 2);
+
+  let speechText = topTwoPositiveResults.join(", and in other news, ");
+
+  if (topTwoPositiveResults.length == 0) {
+    speechText = "Sorry, not a lot of good news today";
+  }
+  
+  return handlerInput.responseBuilder
+    .speak(speechText)
+    .getResponse();
 }
 
 function getHeadlineDescriptions(headlinesResponse) {
